2c6380b9f7834102ad69277144dcc3dc2bc3a14c
Eudis Antonio <perdomoeudis@gmail.com>
2018-11-28 15:00:22 -0500

    UPDATE

--- SQL Server - XML.sql
+++ SQL Server - XML.sql
@@ -1,19 +1,37 @@
---contar la cantidad de elementos Zoom
+--COUNT la cantidad de elementos Zoom
 
 select COUNT(*) from @vxml.nodes('/Reverso/Zoom') T(c)
 
   
---seleccionar el priemer elemento BackGround dentro de Reverso
+--SELECT el priemer elemento BackGround dentro de Reverso
   
 select T.c.value('BackGround[1]', 'varchar(80)') from @vxml.nodes('/Reverso') T(c)
 
 --CREAR xml a partir de una tabla con datos 
---PATH('Data') será el padre de cada registro
---ROOT('tDetail') será la raíz de todo el XML
+    --*PATH('Data') será el padre de cada registro
+    --*ROOT('tDetail') será la raíz de todo el XML
 
 declare @vxml xml
 set @vxml = (
 select top(5)* 
 from production.ProductionOrder
 for XML PATH('Data'), ROOT('tDetail'))
+                           
+--UPDATE 
+DECLARE @res XML = '<Subject>English</Subject>
+<Marks>67</Marks>
+<Subject>Science</Subject>
+<Marks>75</Marks>'
+
+
+DECLARE @student XML = '<Student> 
+   <Name>XYZ</Name>
+   <Roll>15</Roll>
+   <Result />
+   <Attendance>50</Attendance>
+</Student>'
+
+
+SET @student.modify('insert sql:variable("@res") as first into (/Student/Result)[1]')
+                           
 
